782179ac4fcc7f03a4c41be704005a5d1ffcb12e
AmritenduPakhira <pakhiraamritendu@gmail.com>
2024-08-26 17:44:25 +0530

    backend change cors

--- Backend/index.js
+++ Backend/index.js
@@ -33,7 +33,7 @@ let corsOptions = {
   origin : ['https://farmtech-six.vercel.app/'],
 }
 
-app.use(cors( corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
